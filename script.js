c34f0df313587011db3c3c7577f8f60dbe72ed02
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2023-06-29 10:47:42 -0300

    funcionando completamente

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function Insert(num){
 function limpar() {
     document.getElementById('result').innerHTML="";
 }
+function Volta(){
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML= result.substring(0,result.length-1);
+}
+function Calcular(){
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML=eval(result)
+    }else{
+        document.getElementById('result').innerHTML="Nada,Burro!"
+    }
+}
